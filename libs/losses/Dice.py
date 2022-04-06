b375339ec11911e8f3a574fe5b5bc3c0a2e5ba98
Lukas Dauterman <lukas.dauterman@gmail.com>
2022-04-06 10:34:45 -0700

    Update Dice.py

--- libs/losses/Dice.py
+++ libs/losses/Dice.py
@@ -4,6 +4,6 @@ import torch.nn as nn
 class DiceLoss(nn.Module):
     def __init__(self,reduce=True):
         super().__init__()
-        self.loss = FocalTverskyLoss(alpha = .5,gamma=1,reduce = True)
+        self.loss = FocalTverskyLoss(alpha = .5,gamma=1,reduce = reduce)
     def forward(self,predictions,target):
         return self.loss(predictions,target)
