0570268b56cfeb9607af8df6b47ebf0e4f01fd7d
Lukas Dauterman <lukas.dauterman@gmail.com>
2022-04-06 21:17:54 -0700

    pusing more flexible model training

--- libs/training/PL_train.py
+++ libs/training/PL_train.py
@@ -5,7 +5,7 @@ import torch.nn.functional as F
 
 class Main_Loop(pl.LightningModule):
 
-    def __init__(self, model,loss,metric,learning_rate=0.01,num_classes = 2,batch_size = 8):
+    def __init__(self, model,loss,metric,learning_rate=0.01,num_classes = 2,batch_size = 8,scheduler = None,scheduler_args = None):
         super().__init__()
         self.model = model
         self.learning_rate = learning_rate
@@ -13,13 +13,24 @@ class Main_Loop(pl.LightningModule):
         self.num_classes = num_classes
         self.batch_size = batch_size
         self.metric = metric
-
+        self.scheduler = scheduler
+        self.scheduler_args = scheduler_args
     def forward(self, x):
         return self.model(x)
 
     def configure_optimizers(self):
         optimizer = torch.optim.AdamW(self.model.parameters())
-        return optimizer#,[lr_scheduler]
+        if self.scheduler:
+            lr_scheduler = {
+            'scheduler': self.scheduler(optimizer = optimizer,**self.scheduler_args),
+            'interval': 'epoch',
+            'strict': True,
+            'monitor': 'val_acc_epoch',
+            }
+
+            return [optimizer],[lr_scheduler]
+        else:
+            return optimizer
 
     def training_step(self, batch, batch_idx):
 
@@ -35,7 +46,6 @@ class Main_Loop(pl.LightningModule):
             self.log("Train Acc",float(self.metric(pred,targets).cpu().numpy()) , on_epoch=True,on_step = True,batch_size=self.batch_size)
         
         return batch_loss
-
 
     def validation_step(self, batch, batch_idx):
 
@@ -55,6 +65,3 @@ class Main_Loop(pl.LightningModule):
             self.log("Validation Acc",float(self.metric(pred,targets).cpu().numpy()) , on_epoch=True,on_step = True,batch_size=self.batch_size)
 
         return batch_loss
-
-
-
