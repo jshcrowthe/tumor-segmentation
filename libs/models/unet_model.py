66aac48693dd01959c72bc41ea98d2c49800a6db
barrycheng8 <barrycheng8@gmail.com>
2022-04-23 18:39:02 -0700

    clean up comments and unet impl

--- libs/models/unet_model.py
+++ libs/models/unet_model.py
@@ -23,26 +23,23 @@ class Unet(nn.Module):
 
         # Maximum depth 4, otherwise dimensions are too small. Channels must be passed as integers to torch.nn
         self.channel_dims = [int(64 * 2.0 ** (i)) for i in range(self.depth)]
-        # print("Unet channels", self.channel_dims)
 
         self.up_channel = nn.Conv3d(in_channels=1, out_channels=64, kernel_size=1)
         self.down = UnetDown(self.channel_dims)
         self.bottom = ConvBlock(self.channel_dims[-1], self.channel_dims[-1])
         self.up = UnetUp(self.channel_dims)
 
-
-        # Final conv to retain the input volume dimensions
+        # Upscale 
         self.upscale = nn.Sequential(
             nn.Upsample(scale_factor=2),
             ConvBlock(self.channel_dims[1], self.channel_dims[1]),
 
         )
+
+        # Final conv to retain the input volume dimensions
         self.final_conv = nn.Conv3d(self.channel_dims[1], num_classes, 1)
 
     def forward(self, input):
-        _, _, D, H, W = input.shape
-
-        # print("initial input shape", input.shape)
         input = self.up_channel(input)
         out, features = self.down(input)
         out = self.bottom(out)
@@ -60,7 +57,6 @@ class UnetUp(nn.Module):
         # Large -> small
         self.channel_dims = copy.deepcopy(channel_dims)
         self.channel_dims.reverse()
-        # print("up dims", self.channel_dims)
 
         self.up = nn.ModuleList(
             [
@@ -87,15 +83,11 @@ class UnetUp(nn.Module):
         N_i, C_i, D_i, H_i, W_i = input.shape
         N_f, C_f, D_f, H_f, W_f = features.shape
 
-        # print("input shape", input.shape)
-        # print("features shape", features.shape)
-
         # The feature dimensions are always bigger because they come from the input
         d_delta = D_f - D_i
         h_delta = H_f - H_i
         w_delta = W_f - W_i
 
-        # print("D, H, W", d_delta, h_delta, w_delta)
 
         # Return the center cropped volume
         res = features[
@@ -110,18 +102,13 @@ class UnetUp(nn.Module):
 
     def forward(self, input, features):
         features.reverse()
-        # print("feature channels", [f.shape for f in features])
-        # print("up iter step", len(self.up))
+
         for i in range(len(self.up) - 1):
-            # print("iteration #", i)
+            # Perform a transposed convolution to double the volum space
             out = self.conv[i](input)
             down_features = self.center_crop_3d(out, features[i + 1])
-            # print("out shape", out.shape)
-            # print("features shape", down_features.shape)
             out = torch.cat([out, down_features], dim=1)  # Concatenate on the channels
-            # print("out shape", out.shape)
-            # print("expected for shape for upconv", self.channel_dims[i])
-            input = self.up[i](out)
+            input = self.up[i](out) # Perform a forward convolution for the up-tep
 
         return input
 
@@ -147,14 +134,12 @@ class UnetDown(nn.Module):
         # Store and return these features to be fed on the expanding path of Unet
         features = []
 
-        # print("Down iters", len(self.down))
         for f in self.down:
             out = f(input)
             features.append(out)
             # Use the max pooled output for the next layer
             input = self.max_pool(out)
 
-        # print("Down features", [f.shape for f in features])
         return input, features
 
 
@@ -168,7 +153,7 @@ class ConvBlock(nn.Module):
         super(ConvBlock, self).__init__()
 
         self.conv_block = nn.Sequential(
-            # Convolutions are unpadded
+            # Convolutions are padded so we don't lose dimensionality with increasing depth
             nn.Conv3d(in_channels=input_dim, out_channels=output_dim, kernel_size=3, padding=1),
             nn.ReLU(),
             nn.Conv3d(in_channels=output_dim, out_channels=output_dim, kernel_size=3, padding=1),
